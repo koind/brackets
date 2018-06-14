cdbadc3de947cea80e984af24d65dfee9943dae6
Damir15 <krosava_15d@mail.ru>
2018-06-14 20:44:13 +0600

    Added tests

--- tests/BracketsTest.php
+++ tests/BracketsTest.php
@@ -2,9 +2,41 @@
 
 namespace Tests;
 
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
+use Koind\Brackets;
 
 class BracketsTest extends TestCase
 {
+    public function testEmptyString()
+    {
+        $bracket = new Brackets();
+        $this->expectException(InvalidArgumentException::class);
+        $bracket->checkString('');
+    }
 
+    public function testUnacceptableSymbols()
+    {
+        $bracket = new Brackets();
+        $this->expectException(InvalidArgumentException::class);
+        $bracket->checkString('()()()sdf()9)()()()(_)()=()().');
+    }
+
+    public function testIncorrectString()
+    {
+        $bracket = new Brackets();
+        $this->assertEquals(
+            false,
+            $bracket->checkString('()()()()()()()((()()(())()())))()()()()()()()()()')
+        );
+    }
+
+    public function testCorrectString()
+    {
+        $bracket = new Brackets();
+        $this->assertEquals(
+            true,
+            $bracket->checkString('()()()((((()))()()(((()())))))()()(()()())()()()()')
+        );
+    }
 }
